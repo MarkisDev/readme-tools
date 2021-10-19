75eda521d334b16d14bfa0650c32af820984b139
Rijuth Menon <rijuthm@gmail.com>
2021-10-19 20:17:38 +0530

    added entry point for the app

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import loaders from './loaders/index.js';
+import * as routes from './routes/index.js';
+import config from './config/index.js';
+
+async function startServer()
+{
+    const app = express();
+
+    await loaders(app);
+    routes.init(app);
+    app.listen(config.port, (err) =>
+    {
+        if (err)
+        {
+            console.log(err);
+            return;
+        }
+        console.log(`Your server is ready !`);
+    });
+}
+
+startServer();
